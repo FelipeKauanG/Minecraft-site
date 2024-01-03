1616befe28e32dfda3289d3e873f7cfbb3308814
Felipe Kauan <felipekauanprofissional@gmail.com>
2024-01-03 00:21:42 -0300

    v1.5

--- Scripts/userphoto.js
+++ Scripts/userphoto.js
@@ -1,2 +1,17 @@
-const foto = window.document.querySelector("#foto")
+const foto = window.document.getElementById("genero")
+const masc = window.document.querySelector("#masc")
+const femi = window.document.querySelector("#femi")
+const outr = window.document.querySelector("#outr")
+const userphoto = window.document.getElementById("userphoto")
+
+
+masc.addEventListener("change", function(){
+    foto.style.backgroundImage = 'url("../assets/Login/Male.png")'
+})
+femi.addEventListener("change", function(){
+    foto.style.backgroundImage = 'url("../assets/Login/Female.png")'
+})
+outr.addEventListener("change", function(){
+    foto.style.backgroundImage = 'url("../assets/Login/Intersex.png")'
+})
 
